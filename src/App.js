bda50cd62820e17a255caa9064a79a1b6f4494f3
Chris Pachomski <cpachomski@gmail.com>
2018-10-25 00:11:44 -0400

    utilize new modules in app root

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+
+import Themer from './pattern/themer';
+import { Container } from './pattern/structure';
+import Header from './components/header';
 import ItemList from './containers/item-list';
 
 class App extends Component {
   render() {
-    return <ItemList />;
+    return (
+      <Themer>
+        <Container>
+          <Header logoText="HN" />
+          <ItemList />
+        </Container>
+      </Themer>
+    );
   }
 }
 
